f1060284ca7455604e2b8d881c67eee548e5a086
Anomitra <anomitra1993@gmail.com>
2017-11-26 18:47:46 +0530

    [FIX] Minor changes, added migrations

--- transactions/views.py
+++ transactions/views.py
@@ -5,5 +5,7 @@ from rest_framework.viewsets import ModelViewSet
 
 
 class TransactionViewSet(ModelViewSet):
+
+    queryset = models.Transaction.objects.all()
     model = models.Transaction
     serializer_class = serializers.TransactionSerializer
